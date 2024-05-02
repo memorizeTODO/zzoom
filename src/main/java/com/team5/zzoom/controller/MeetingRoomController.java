2678a53bc8538853a45c4b74864a17c065ec4796
memorizeTODO <memorizeTODO@gmail.com>
2024-05-02 21:07:13 +0900

    사이드 바 디자인 변경

--- src/main/java/com/team5/zzoom/controller/MeetingRoomController.java
+++ src/main/java/com/team5/zzoom/controller/MeetingRoomController.java
@@ -1,7 +1,11 @@
 package com.team5.zzoom.controller;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +14,9 @@ public class MeetingRoomController {
 
 	@RequestMapping("/meetingroom")
 	public String test(Model model) {
-
+		
 		return "meetingroom";
 	}
+	
+	//@GetMapping("get")
 }
