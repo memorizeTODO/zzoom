fef5ab3d707355e4a78918d2f4a5e9096f258141
memorizeTODO <162887005+memorizeTODO@users.noreply.github.com>
2024-05-24 05:50:03 +0900

    모든 외부 ip의 접근을 허용하도록 변경

--- src/main/java/com/team5/zzoom/security/WebConfig.java
+++ src/main/java/com/team5/zzoom/security/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 //.allowedOrigins("campscore.c7essawq6vkw.ap-northeast-2.rds.amazonaws.com")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")
